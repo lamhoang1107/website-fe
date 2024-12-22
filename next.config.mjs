7f6bcdd1c73488a783064ef5805406ac892882e5
lamhoang1107 <hoanglam110799@gmail.com>
2024-12-22 21:53:34 +0700

    update eslint

--- next.config.mjs
+++ next.config.mjs
@@ -10,6 +10,11 @@ const nextConfig = {
   rules: {
     "@next/next/no-img-element": "off",
    },
+   eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
